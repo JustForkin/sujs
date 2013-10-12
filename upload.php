d83f0596abb3215c73fd12898e276b8fa13f50b1
Eric <ewust@umich.edu>
2013-10-12 03:28:41 -0400

    i know how to program

--- upload.php
+++ upload.php
@@ -9,7 +9,7 @@ $data = file_get_contents("php://input");
 
 do {
 	$filename = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-"), 0, 12);
-while (file_exists("$data_dir/$filename"));
+} while (file_exists("$data_dir/$filename"));
 
 file_put_contents("$data_dir/$filename", $data);
 
