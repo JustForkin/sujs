6f3614b9f18ff6cca61138cc1666fb43feef6d4b
Eric <ewust@umich.edu>
2013-10-12 03:46:28 -0400

    one step at a time

--- download.php
+++ download.php
@@ -1,6 +1,18 @@
 <?
 
-print_r($_GET);
+// Be sure to put an .htaccess file like this in your directory:
+/*
+RewriteEngine On
+
+RewriteCond %{HTTP_HOST} hobocomp.com
+RewriteCond %{REQUEST_URI} !^/sujs/$
+RewriteRule ^(.*)$ download.php?fname=$1 [L,QSA]
+*/
+
+
+$fname = preg_replace("/[^a-zA-Z0-9_-]+/", "", $_GET["fname"]);
+
+echo $fname
 
 
 ?>
